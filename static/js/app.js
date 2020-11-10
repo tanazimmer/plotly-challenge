7774a733f6d98b68bebded3ceeeca6814710e14b
tanazimmer <68484974+tanazimmer@users.noreply.github.com>
2020-11-09 21:39:14 -0500

    initializing and pseudocode

---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+// create a function
+    
+    // get data
+        // filter by id
+
+        // top 10
+
+        // reformat
+
+        // top 10
+        
+        //  bar pplot
+        
+        // bubble
+
+        // pie
+}
+    
+// display metadata
+//function
+  //get data
+        
+        // get the metadata info for the demographic panel
+
+        // filter
+
+        // demos
+        
+        // clear table
+
+        // append
+}
+
+// function for change data
+}
+
+// get data
+    // select dropdown menu 
+
+        // id
+
+init();
